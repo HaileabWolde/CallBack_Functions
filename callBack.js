00103e15cb39b169e7918318f83e0b1eec5c3c38
Haileab <womahaileab1428@gmail.com>
2025-10-19 22:46:07 +0300

    Added examples for callbackHell, async/await, promieses

--- callBack.js
+++ callBack.js
@@ -1,19 +1,76 @@
+const { resolve } = require("path");
+
+/* callBack Hell Example
 function TaskOne(callback) {
 setTimeout(()=>{
 console.log("TaskOne is Completed")
-}, 10)
 callback()
+}, )
+
+}
+function TaskThree(callback){
+      setTimeout(()=> {
+            console.log("TaskThree is Completed")
+            callback();
+      },)
 }
 function TaskTwo(callback) {
  setTimeout(()=> {
     console.log("TaskTwo is Completed")
- }, 50)
- callback()
+    callback()
+ }, )
+
 }
 TaskOne( function(){
-      TaskTwo(()=> console.log("Both Functions are Completed"))
+      TaskTwo(function(){
+             TaskThree(()=> console.log("All of the Taskes are Finshied"))
+      })
 }
-)
-
+)*/
+/* Using Promisies
+var p = new Promise(function(resolve, reject){
+      setTimeout(()=>{
+            console.log("TaskOne is Completed")
+      }, 10)
+      resolve();
+})
+p.then(function(){
+      setTimeout(()=> {
+            console.log("TaskTwo is Completed")
+      }, 10)
+      resolve()
+}).then(function(){
+      setTimeout(()=> {
+            console.log("TaskThree is Completed")
+      }, 10)
+      resolve()
+}).then(function(){
+      setTimeout(()=> {
+            console.log("All Task aree completed")
+      }, 10)
+})
+      */
+async function TaskOne() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Complete TaskOne");
+            resolve();
+        }, 1000);
+    });
+}
+function TaskTwo(){
+      console.log('Complete TaskedTwo')
+}
+function TaskThree(){
+      console.log("Complete TaskThree")
+}
+async function getTasks(){
+       await TaskOne();
+       await TaskTwo();
+       await TaskThree();
+       console.log("All Task are completed");
+}
+getTasks()
+console.log('Fuck Off')
 
 
